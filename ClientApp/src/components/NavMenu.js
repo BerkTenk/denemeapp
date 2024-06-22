d01943258b40a39e6d29b71f2f4e128b0018b9c3
Berk Tenkoğlu <berk.tenkoglu@gmail.com>
2024-06-22 12:14:56 +0300

    update for navbar

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
-import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink, UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem, } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
 import Logo from "../assets/logo.png"
+
 
 export class NavMenu extends Component {
   static displayName = NavMenu.name;
@@ -31,15 +35,31 @@ export class NavMenu extends Component {
           </NavbarBrand>
           <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
           <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
-            <ul className="navbar-nav flex-grow display-6">
+            <ul className="navbar-nav flex-grow ">
+            <UncontrolledDropdown nav inNavbar className="me-2">
+              <DropdownToggle nav caret className ="btn btn-outline-success">
+                OUR THERAPIES
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>Individual Therapy</DropdownItem>
+                <DropdownItem>Couples Therapy</DropdownItem>
+                <DropdownItem>Consult a social worker online</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
               <NavItem>
-                <NavLink tag={Link} className="text-dark btn btn-outline-success me-3" to="/">Home</NavLink>
+                <NavLink tag={Link} className="text-dark btn btn-outline-success me-2" to="/register">REGISTER</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink tag={Link} className="text-dark btn btn-outline-success me-3" to="/register">Register</NavLink>
+                <NavLink tag={Link} className="text-dark btn btn-outline-success me-2" to="/calendar">CALENDAR</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink tag={Link} className="text-dark btn btn-outline-success" to="/calendar">Calendar</NavLink>
+                <NavLink tag={Link} className="text-dark btn btn-outline-success me-2" to="/">BLOG</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Link} className="text-dark btn btn-outline-success me-2" to="/">JOIN US</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Link} className="text-dark btn btn-outline-success me-2" to="/">RATES</NavLink>
               </NavItem>
             </ul>
           </Collapse>
